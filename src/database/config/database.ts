bfb5ee45f559d3f81f8229933192757cedd25220
carolhn <nunescaroline905@gmail.com>
2025-01-04 21:39:27 -0300

    feat: add server initialization script with database connection

---
+++ src/database/config/database.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
+dotenv.config();
+
+const { PG_HOST, PG_PORT, PG_USER, PG_PASSWORD, PG_DATABASE } = process.env;
+
+const sequelize = new Sequelize({
+  host: PG_HOST || 'localhost',
+  port: Number(PG_PORT) || 5432,
+  username: PG_USER || 'root',
+  password: PG_PASSWORD || 'password',
+  database: PG_DATABASE || 'loja-agilstore',
+  dialect: 'postgres',
+  logging: true,
+});
+
+export default sequelize;
