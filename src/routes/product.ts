a678d041ae2cc9b05e8da04c6cae9fee93855580
carolhn <nunescaroline905@gmail.com>
2025-01-05 22:24:14 -0300

    feat: create new route delete product

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { createProductController, listProductController, updateProductController } from '../controllers/product';
+import {
+  createProductController,
+  deleteProductController,
+  listProductController,
+  updateProductController,
+} from '../controllers/product';
 
 const productsRouter = Router();
 
 productsRouter.post('/new_product', createProductController);
 productsRouter.get('/list_product', listProductController);
 productsRouter.put('/update_product/:id', updateProductController);
+productsRouter.delete('/remove_product/:id', deleteProductController);
 
 export default productsRouter;
