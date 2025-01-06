4bce12a503fa97be6cec3bd07a6d6990bb197936
carolhn <nunescaroline905@gmail.com>
2025-01-05 23:21:39 -0300

    feat: create new route search product

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -3,6 +3,7 @@ import {
   createProductController,
   deleteProductController,
   listProductController,
+  searchProductController,
   updateProductController,
 } from '../controllers/product';
 
@@ -12,5 +13,6 @@ productsRouter.post('/new_product', createProductController);
 productsRouter.get('/list_product', listProductController);
 productsRouter.put('/update_product/:id', updateProductController);
 productsRouter.delete('/remove_product/:id', deleteProductController);
+productsRouter.get('/search', searchProductController);
 
 export default productsRouter;
